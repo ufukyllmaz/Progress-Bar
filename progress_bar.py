af9cf29e831a317cfeb41b9333bef2ddcdf2568d
ufukyllmaz <70961770+ufukyllmaz@users.noreply.github.com>
2023-10-19 17:50:41 +0300

    Update progress_bar.py

--- progress_bar.py
+++ progress_bar.py
@@ -1,11 +1,11 @@
-class ProgresBar:
-  def __init__(self):
+class ProgressBar:
+  def __init__(self, process_bar_size):
     self.progress_bar_count = 1
-    self.progress_bar = <bar_size>
+    self.progress_bar = process_bar_size
 
   def progress_bar_method(self):
     self.progress_bar_perc = (self.progress_bar_count / self.progress_bar) * 100
-    print("Process Bar : [" + int(50 * self.progress_bar_perc/100) * "|" + (50 - int(50*self.progress_bar_perc/100)) * ' ' +"] " + '%' + str(int(self.progress_bar_perc))
+    print("Processing : [" + int(50 * self.progress_bar_perc/100) * "|" + (50 - int(50*self.progress_bar_perc/100)) * ' ' +"] " + '%' + str(int(self.progress_bar_perc))
     self.progress_bar_count += 1
 
   def execute(self):
