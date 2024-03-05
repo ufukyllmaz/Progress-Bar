17eb4932913c9245d6105d0a4ab72fb75d31234f
Ufuk YILMAZ <70961770+ufukyllmaz@users.noreply.github.com>
2024-03-06 01:02:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import time
 if __name__ == '__main__':
   process_size = 126
   for i in range(process_size + 1):
+    """
+    Runs for each step
+    """
     progress_bar_display = ProgressBar(i, process_size).execute()
     print(progress_bar_display, end='', flush=True)
     time.sleep(0.2)
